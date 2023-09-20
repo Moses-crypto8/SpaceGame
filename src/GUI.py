f45c62e756dddde4085b51fcf55d523edd854d62
Moses  Hernandez <moseshernandez@Mosess-MacBook-Air-2.local>
2023-09-20 10:14:30 -0600

    update dimensions

--- src/GUI.py
+++ src/GUI.py
@@ -7,12 +7,12 @@ class GUI:
         self.display_frame = LabelFrame(self.root, bg="green", fg="white", padx=7)
         
         # Displaying the frame1 in row 0 and column 0
-        self.display_frame.grid(row=0, column=0, ipadx=355, ipady=150)
+        self.display_frame.grid(row=0, column=0, ipadx=400, ipady=150)
         # Constructing the second frame, frame2
         self.interactive_frame = LabelFrame(self.root, bg="red", padx=1)
         
         # Displaying the frame2 in row 0 and column 1
-        self.interactive_frame.grid(row=1, column=0, ipadx=355, ipady=85)
+        self.interactive_frame.grid(row=1, column=0, ipadx=400, ipady=115)
         
         self.root.mainloop()
         self.description_text = Label()
@@ -28,8 +28,8 @@ root = Tk()
 # root window title and dimension
 root.title("Space Game")
 # Set geometry (widthxheight)
-root.geometry('800x567')
-#root.resizable(False, False)
+root.geometry('800x525')
+root.resizable(False, False)
 
 gui = GUI(root)
 
